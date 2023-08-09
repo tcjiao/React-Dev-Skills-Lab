77088c9d23dae56c40f7ca1e10634a8e860b9b5b
zeusjiaotianci <zeusjiaotianci@gmail.com>
2023-08-09 16:46:45 -0700

    part3

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import './App.css'
 import SkillList from './SkillList'
 import SkillListItem from './SkillListItem';
-
-const skills = ["HTML", "CSS", "Javascript", "Python"];
+import { useState } from 'react';
 
 export default function App() {
+  const [skills, setSkills] = useState([
+    { name: "HTML", level: 5}, 
+    { name: "CSS", level: 3}, 
+    { name: "Javascript", level: 4}, 
+    { name: "Python", level: 2}
+  ]);
+  const [showSkills, setShowSkills] = useState(true);
+
+  function handleAddSkill() {
+    const newSkills = [...skills, 'xxxx';]
+    setSkills(newSkills);
+  }
 
   return (
     <div className='App'>
       <h1>React Dev Skills</h1>
-      <SkillList skills={skills} />
+      {showSkills && <SkillList skills={skills} />}
+      <button onClick={handleAddSkill}>Add Skills</button>
     </div>
   )
 }
--- src/SkillListItem.jsx
+++ src/SkillListItem.jsx
@@ -4,9 +4,6 @@ export default function SkillListItem({ skill, index }) {
     return (
     <li
     className="SkillListItem"
-    style={{
-        backgroundColor: index % 2 ? "green" : "grey"
-    }}
-    > {skill} </li>
+    > {skill.name} <div>LEVEL{skill.level}</div></li>
     );
 }
